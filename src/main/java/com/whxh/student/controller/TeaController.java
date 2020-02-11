583aed76a11aa8638cf9a332e77af08b28241210
test1 <565877201@qq.com>
2020-02-11 15:20:35 +0800

    yj新增功能：查询所有老师

---
+++ src/main/java/com/whxh/student/controller/TeaController.java
@@ -0,0 +1,13 @@
+package com.whxh.student.controller;
+
+/**
+ * Created By IDEA
+ *
+ * @Author: YJ
+ * @Date: 2020/2/11
+ * @Time: 15:11
+ **/
+public class TeaController {
+  //yj开发查询老师业务
+
+}
---
+++ src/main/java/com/whxh/student/controller/Test2.java
@@ -0,0 +1,11 @@
+package com.whxh.student.controller;
+
+/**
+ * Created By IDEA
+ *
+ * @Author: YJ
+ * @Date: 2020/2/11
+ * @Time: 15:14
+ **/
+public class Test2 {
+}
